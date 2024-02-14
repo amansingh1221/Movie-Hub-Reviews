8aab83f53bd60b25980431a90dfab45d4afd729a
Aman Singh <amansingh6807@gmail.com>
2024-02-14 23:08:37 +0530

    initial commit

---
+++ Movies/src/main/java/com/aman/Moivies/MoiviesApplication.java
@@ -0,0 +1,17 @@
+package com.aman.Moivies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+
+public class MoiviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoiviesApplication.class, args);
+	}
+
+
+}
---
+++ Movies/src/test/java/com/aman/Moivies/MoiviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aman.Moivies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoiviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
